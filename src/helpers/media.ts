ab74b65ba83c9c969e376aa9e779f3fc1604422b
Dmitry <dmi3.shishkin@gmail.com>
2022-12-12 16:46:03 +0300

    feat: fixed getting correct images

--- src/helpers/media.ts
+++ src/helpers/media.ts
@@ -4,13 +4,12 @@ export const getCourseThumbnail = (media: any, size: string) => {
   }
 
   const id = media[0]?.id;
-  const fileName = media[0]?.file_name.split('.').shift();
   const fileExtension = media[0]?.file_name.split('.').pop();
+  const fileName = media[0]?.file_name.replace('.' + fileExtension, '');
   let fileThumbnail = '';
 
   if (media[0]?.generated_conversions.hasOwnProperty('thumb_' + size)) {
     fileThumbnail = '-thumb_' + size;
   }
-
   return `https://author.edston.com/storage/media/${id}/conversions/${fileName}${fileThumbnail}.${fileExtension}`;
 };
