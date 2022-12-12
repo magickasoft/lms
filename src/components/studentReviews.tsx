f985a02d49f533d9ed45158a9422c968247d8ed9
Evgeny Shmakov <sjs-master@yandex.ru>
2022-12-12 19:15:15 +0700

    fix: studentReviews

--- src/components/studentReviews.tsx
+++ src/components/studentReviews.tsx
@@ -117,32 +117,36 @@ const RatingTitle = SC.div`
 
 const data = [
   {
-    instagram: "@dBarnes · 7 March 2022",
-    faadback: "A wonderful, healthy pastime! Great, exciting and giving hope for a full perception of the world in its best light! Developing motor skills-we develop acute and quick problem solving! Great! Thanks!",
-    src: "/images/student_reviews_1.png",
-    name: "Tara Brangston",
-    label: "Online entrepreneur"
+    instagram: '@dBarnes · 7 March 2022',
+    faadback:
+      'A wonderful, healthy pastime! Great, exciting and giving hope for a full perception of the world in its best light! Developing motor skills-we develop acute and quick problem solving! Great! Thanks!',
+    src: '/images/student_reviews_1.png',
+    name: 'Tara Brangston',
+    label: 'Online entrepreneur',
   },
   {
-    instagram: "@Mikeeeee · 5 March 2022",
-    faadback: "Now I feel my inner power After this program I does not feel my daily weakness without taking any pills and medications. This is unbelieveble!",
-    src: "/images/student_reviews_2.png",
-    name: "Mike Thomson",
-    label: "Nutrition Coach"
+    instagram: '@Mikeeeee · 5 March 2022',
+    faadback:
+      'Now I feel my inner power After this program I does not feel my daily weakness without taking any pills and medications. This is unbelieveble!',
+    src: '/images/student_reviews_2.png',
+    name: 'Mike Thomson',
+    label: 'Nutrition Coach',
   },
   {
-    instagram: "@BrianAl · 9 April 2022",
-    faadback: "Hello. Thank you so much for the course program!!! It was a pleasure, I liked everything very much!!! The result speaks better than any words!",
-    src: "/images/student_reviews_3.png",
-    name: "Brian Alexander",
-    label: "Successful businessman"
+    instagram: '@BrianAl · 9 April 2022',
+    faadback:
+      'Hello. Thank you so much for the course program!!! It was a pleasure, I liked everything very much!!! The result speaks better than any words!',
+    src: '/images/student_reviews_3.png',
+    name: 'Brian Alexander',
+    label: 'Successful businessman',
   },
   {
-    instagram: "@NicGray · 7 July 2022",
-    faadback: "I've been doing it for 2 months, 3 times a week - the result is minus 8 cm in the waistline. Very cool result! Thank you for this useful course.",
-    src: "/images/student_reviews_4.png",
-    name: "Nicole Gray",
-    label: "Novice disco dancer"
+    instagram: '@NicGray · 7 July 2022',
+    faadback:
+      "I've been doing it for 2 months, 3 times a week - the result is minus 8 cm in the waistline. Very cool result! Thank you for this useful course.",
+    src: '/images/student_reviews_4.png',
+    name: 'Nicole Gray',
+    label: 'Novice disco dancer',
   },
 ];
 
@@ -151,42 +155,35 @@ export const StudentReviews = (props: any) => {
 
   return (
     <Element name="StudentReviews">
-        <Container>
-          <CenterBlock>
-                <BoldText>Student reviews</BoldText>
-                <Title>about our courses</Title>
+      <Container>
+        <CenterBlock>
+          <BoldText>Student reviews</BoldText>
+          <Title>about our courses</Title>
 
-                <NormalTitle>All our students are delighted with the results and the work of online trainers</NormalTitle>
+          <NormalTitle>All our students are delighted with the results and the work of online trainers</NormalTitle>
 
-                <Grid container >
-                  {data.map(o => (
-                    <Grid key={o.name} item xs={12} sm={12} md={12} lg={6} xl={6}>
-                      <Item>
-                        <Rating stars={5} />
-                        <RatingTitle>{o.instagram}</RatingTitle>
-                        <StudentFeedbackText>
-                          {o.faadback}
-                        </StudentFeedbackText>
-                        <FeedbackRow>
-                          <Avatar>
-                            <Image
-                              src={o.src}
-                              height={80}
-                              width={80}
-                              alt="Student review image"
-                            />
-                          </Avatar>
-                          <Info>
-                            <UserName>{o.name}</UserName>
-                            <UserTitle>{o.label}</UserTitle>
-                          </Info>
-                        </FeedbackRow>
-                      </Item>
-                    </Grid>
-                  ))}
-                </Grid>
-          </CenterBlock>
-        </Container>
+          <Grid container>
+            {data.map(o => (
+              <Grid key={o.name} item xs={12} sm={12} md={12} lg={6} xl={6}>
+                <Item>
+                  <Rating stars={5} />
+                  <RatingTitle>{o.instagram}</RatingTitle>
+                  <StudentFeedbackText>{o.faadback}</StudentFeedbackText>
+                  <FeedbackRow>
+                    <Avatar>
+                      <Image src={o.src} height={80} width={80} alt="Student review image" />
+                    </Avatar>
+                    <Info>
+                      <UserName>{o.name}</UserName>
+                      <UserTitle>{o.label}</UserTitle>
+                    </Info>
+                  </FeedbackRow>
+                </Item>
+              </Grid>
+            ))}
+          </Grid>
+        </CenterBlock>
+      </Container>
     </Element>
   );
 };
